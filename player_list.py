a865381219250ecbf3afa7cf8b96885c35618986
VasilAshrafzyanov <vasil-vassaby@yandex.ru>
2023-01-17 10:08:17 +0300

    added solution to file player_list.py

--- player_list.py
+++ player_list.py
@@ -1,6 +1,10 @@
-def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
+def get_player(player_count):
+    """
+    Формирование списка имен игроков
+    Параметр: player_count - количество игроков
+    """
+    players = []
+    for i in range(player_count):
+        player_name = input(f'Введите имя игрока {i + 1}: ')
+        players.append(player_name)
+    return players
