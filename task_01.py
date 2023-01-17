43c20098a6ec57c964547e267e341a62dc92c9f3
VasilAshrafzyanov <vasil-vassaby@yandex.ru>
2023-01-17 09:36:58 +0300

    added solution to file winner.py

--- task_01.py
+++ task_01.py
@@ -30,32 +30,7 @@ def get_player(player_count):
     return players
 
 
-def find_winner(players, count_candies):
-    """
-    Определение и вывод победителя
-    Параметры: players - список имен игроков, count_candies - количество конфет
-    """
-    is_winner = False
-    winner_name = None
-    while not is_winner:
-        for player in players:
-            active = False
-            while not active:
-                print(f'Ход игрока ' + player.title())
-                player_number = int(input('Введите количество конфет: '))
-                if player_number <= 28:
-                    active = True
-                    count_candies = count_candies - player_number
-                    if count_candies == 0:
-                        is_winner = True
-                        winner_name = player
-                        break
-                    break
-                else:
-                    print('Конфет должно быть не более чем 28!')
-            print('Остаток конфет: ' + str(count_candies))
-    else:
-        return winner_name.title()
+
 
 
 number_candies = get_candy()
